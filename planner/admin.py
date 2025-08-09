a84d80fd28c648fa972f5647d81eec8c1fc041ac
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-08-09 16:57:00 +0200

    Register models in Django admin

--- planner/admin.py
+++ planner/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Budget, Operation, Debt, SavingCategory, Savings, Portfolio, Instrument, CurrencyRate, StockPrice
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Budget)
+admin.site.register(Operation)
+admin.site.register(Debt)
+admin.site.register(SavingCategory)
+admin.site.register(Savings)
+admin.site.register(Portfolio)
+admin.site.register(Instrument)
+admin.site.register(CurrencyRate)
+admin.site.register(StockPrice)

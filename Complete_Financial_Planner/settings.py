1fd72a641142aaac3c42fb5db1cccfca5c1173e3
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-08-11 00:49:58 +0200

    feat(config): add staticfiles dirs, crispy template pack and planner app route

--- Complete_Financial_Planner/settings.py
+++ Complete_Financial_Planner/settings.py
@@ -118,9 +118,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'planner.CustomUser'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+

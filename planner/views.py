31034927857fa147e000cd684c01fd8a6775f0a0
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-08-11 00:51:11 +0200

    feat(planner): add index view with basic context data

--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import CustomUser, Budget
 
-# Create your views here.
+def index(request):
+    num_inmates = CustomUser.objects.all().count()
+    summary_savings = 0 # on start
+    summary_debts = 0 #  Budget.total_debt.all().count() # add to model budget total_debt
+    summary_investments = 0 # add to model budget total_investments
+    num_visit = request.session.get('num_visit', 0)
+    request.session['num_visit'] = num_visit + 1
+    context = {
+        "num_inmates": num_inmates,
+        "summary_savings": summary_savings,
+        "summary_debts": summary_debts,
+        "summary_investments": summary_investments,
+        "num_visit": num_visit + 1,
+    }
+    return render(request,'planner/index.html', context)

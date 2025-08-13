3bc8c55d0eeae61beaec0dc7467a1f87cc188831
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-08-13 11:27:33 +0200

    feat(operations): add operations list view with template, urls, and styles

--- planner/urls.py
+++ planner/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from planner.views import index, InmatesListView, InmatesDetailView, UserSettingsView, ContactMessageView
+from planner.views import index, InmatesListView, InmatesDetailView, UserSettingsView, ContactMessageView, OperationsListView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -7,6 +7,7 @@ urlpatterns = [
     path("inmates/<int:pk>", InmatesDetailView.as_view(), name="inmates_detail"),
     path("settings/", UserSettingsView.as_view(), name="user_settings"),
     path("contact/", ContactMessageView.as_view(), name="contact"),
+    path("operations/", OperationsListView.as_view(), name="operations"),
 ]
 
 app_name = "planner"
--- planner/views.py
+++ planner/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-
-from .models import CustomUser
+from django.db.models import Sum
+from .models import CustomUser, Operation
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
@@ -13,16 +13,24 @@ def index(request):
     num_inmates = CustomUser.objects.all().count()
     summary_savings = 0  # on start
     summary_debts = 0
-    # Budget.total_debt.all().count() # add to model budget total_debt
+    income_sum = Operation.objects.filter(category__type='INCOME').aggregate(total=Sum('amount'))['total'] or 0
+
+    expense_sum = Operation.objects.filter(category__type='EXPENSE').aggregate(total=Sum('amount'))['total'] or 0
+
+
     summary_investments = 0  # add to model budget total_investments
     num_visit = request.session.get('num_visit', 0)
     request.session['num_visit'] = num_visit + 1
+
     context = {
         "num_inmates": num_inmates,
+        "income_sum": income_sum,
+        "expense_sum": expense_sum,
         "summary_savings": summary_savings,
         "summary_debts": summary_debts,
         "summary_investments": summary_investments,
         "num_visit": num_visit + 1,
+        "surplus_deficit": (income_sum - expense_sum)
     }
     return render(request, 'planner/index.html', context)
 
@@ -56,3 +64,8 @@ class ContactMessageView(generic.FormView):
             form.instance.user = self.request.user
         form.save()
         return super().form_valid(form)
+
+class OperationsListView(LoginRequiredMixin, generic.ListView):
+    model = Operation
+    template_name = "planner/operations_list.html"
+    context_object_name = "operations"
